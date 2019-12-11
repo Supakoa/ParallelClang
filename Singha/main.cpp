c85e5e7a09de6b140556f0b2f1a283f22f7e06d3
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-12-11 15:44:27 +0700

    asd

--- Singha/main.cpp
+++ Singha/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include<stdio.h>
-#include<omp.h>
-#define MAX_BUFFER 10
+#include <stdio.h>
+#include <omp.h>
+#define MAX_BUFFER 100
 #include <queue>
 using namespace std;
 
@@ -10,10 +10,10 @@ class Sushi
 public:
     string name;
     int price;
-    Sushi(string n,int p);
-
+    Sushi(string n, int p);
 };
-Sushi::Sushi(string n,int p){
+Sushi::Sushi(string n, int p)
+{
     name = n;
     price = p;
 }
@@ -21,40 +21,72 @@ class Buffer
 {
 public:
     queue<Sushi> buffer;
-    int n=0;
-    Buffer(){
-        cout<<"eiei";
+    int n = 0;
+    Buffer()
+    {
     }
-    bool isFull(){
-        return buffer.size()>=MAX_BUFFER;
+    bool isFull()
+    {
+        return buffer.size() >= MAX_BUFFER;
     }
-    bool isEmtry(){
-        return n==0;
+    bool isEmtry()
+    {
+        return n == 0;
     }
 
-    void add(Sushi s){
-        while(isFull());
+    void add(Sushi s)
+    {
+        while (isFull())
+            ;
+        cout << s.name << " : " << s.price << "\n";
         buffer.push(s);
     }
 
-    Sushi get(){
-         while(isEmtry());
-         Sushi s = buffer.front();
-         buffer.pop();
-         return s;
+    Sushi get()
+    {
+        while (isEmtry())
+            ;
+        Sushi s = buffer.front();
+        buffer.pop();
+        return s;
     }
-
 };
+class Producer
+{
+
+public:
+    string name;
+    Buffer buffer;
+    Producer(string n, Buffer b);
+};
+
+Producer::Producer(string n, Buffer b)
+{
+    name = n;
+    buffer = b;
+}
 
 int main()
 {
 
-    Buffer a ;
-    #pragma omp parallel
+    Buffer buffer;
+    Producer p1("singha", buffer);
+    Sushi a("name", 50);
+#pragma omp sections
     {
-    printf("Hello world! %d \n ",omp_get_thread_num());
+#pragma omp section
+        {
+            a.price = a.price + 1;
+            p1.buffer.add(a);
+        }
     }
 
+    // printf(" %s\n", p1.name);
+    // cout << p1.name << "\n";
+    // #pragma omp parallel
+    // {
+    // printf("Hello world! %d \n",omp_get_thread_num());
+    // }
 
     return 0;
 }
