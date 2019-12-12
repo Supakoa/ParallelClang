d19857164435521a7e1ddd2fa389b5a427d94315
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-12-12 13:46:17 +0700

    pp

--- temp/main.cpp
+++ temp/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <omp.h>
 #include <fstream>
+#include "../Singha/buffer.cpp"
 using namespace std;
 
 int main()
@@ -11,19 +12,21 @@ int main()
   string sushi[23][2];
   int i = 0, c = 0;
 
-  while (csv.good())
+  while (c <= 45)
   {
     string line;
     // cout << i << " : " << c % 2 << "\n";
     getline(csv, sushi[i][c % 2], ',');
     // sushi[i][c % 2]
-    cout << sushi[i][c % 2] << "\n\n";
+    cout << "[" << i << " : " << c % 2 << "]" << ":" << sushi[i][c % 2] << "\n\n";
     if (c % 2 != 0)
     {
       i++;
     }
     c++;
   }
+
+  cout << c << "\n";
 
   cout << sushi[0][0] << "\n";
 
